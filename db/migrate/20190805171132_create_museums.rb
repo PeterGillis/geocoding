51aef3c067efb62e4eb2104c99ef97ecb2c5520b
Peter Gillis <petergillis@hotmail.com>
2019-08-05 19:21:23 +0200

    model and controller created

---
+++ db/migrate/20190805171132_create_museums.rb
@@ -0,0 +1,12 @@
+class CreateMuseums < ActiveRecord::Migration[5.2]
+  def change
+    create_table :museums do |t|
+      t.string :name
+      t.string :address
+      t.float :latitude
+      t.float :longitude
+
+      t.timestamps
+    end
+  end
+end
