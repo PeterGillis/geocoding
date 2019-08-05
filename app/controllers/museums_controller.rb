5a8dd389962ca3f218d78c2329e40e6d49390d93
Peter Gillis <petergillis@hotmail.com>
2019-08-05 21:02:11 +0200

    updated routes and controller

--- app/controllers/museums_controller.rb
+++ app/controllers/museums_controller.rb
@@ -1,2 +1,19 @@
 class MuseumsController < ApplicationController
+
+  def index
+    @museums = Museum.all
+    render json: @museums
+  end
+
+  lat = params[:lat]
+  lng = params[:lng]
+
+  # Call mapbox - give the coords + 'museums'
+  "https://api.mapbox.com/geocoding/v5/mapbox.places/museums?lat=#{lat}&lng=#{lng}.json"
+  # get mapbox results
+  # Return the results as a json
+
+  def museum_params
+    params.require(:museum).permit(:name, :address, :lat, :lng, :poi)
+  end
 end
