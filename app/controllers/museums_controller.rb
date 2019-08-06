6dfd22445681fb4eb6e89d88c592fde069e13a76
Peter Gillis <petergillis@hotmail.com>
2019-08-06 14:32:15 +0200

    testing method call types

--- app/controllers/museums_controller.rb
+++ app/controllers/museums_controller.rb
@@ -2,27 +2,39 @@
 
 class MuseumsController < ApplicationController
   def index
-    mapbox_key = ENV['MAPBOX_API_KEY']
-    url = "https://api.mapbox.com/geocoding/v5/mapbox.places/-73.989,40.733.json?types=poi&access_token=#{mapbox_key}"
-    # url = "https://api.mapbox.com/geocoding/v5/mapbox.places/museums?lat=#{latitude}&lng=#{longitude}&access_token=#{mapbox_key}"
-
-    response = RestClient.get(url)
-    json = JSON.parse(response)
-    render json: {
-
-      }, status: 200
+    @museums = Museum.all
+    respond_to do |format|
+      format.html
+      format.json { render json:
+        params[:lat]
+      }
   end
+  #   mapbox_key = ENV['MAPBOX_API_KEY']
+  #   url = "https://api.mapbox.com/geocoding/v5/mapbox.places/-73.989,40.733.json?types=poi&access_token=#{mapbox_key}"
+  #   # url = "https://api.mapbox.com/geocoding/v5/mapbox.places/museums?lat=#{latitude}&lng=#{longitude}&access_token=#{mapbox_key}"
 
-  # lat = params[:lat]
-  # lng = params[:lng]
+  #   # response = RestClient.get(url)
+  #   # json = JSON.parse(response)
+  #   # render json: {
+  #   #   request.params[:lat]
+  #   # }
 
-  # Call mapbox - give the coords + 'museums'
-  # make json show on screen then make dynamic.
-  # "https://api.mapbox.com/geocoding/v5/mapbox.places/museums?lat=#{lat}&lng=#{lng}.json"
-  # get mapbox results
-  # Return the results as a json
+  #   museum_serialized = open(url).read
+  #   museum = JSON.parse(museum_serialized)
+
+  #   puts "#{museum['lat']} - #{museum['access_token']}"
+
+  # end
 
 
+  # # lat = params[:lat]
+  # # lng = params[:lng]
+
+  # # Call mapbox - give the coords + 'museums'
+  # # make json show on screen then make dynamic.
+  # # "https://api.mapbox.com/geocoding/v5/mapbox.places/museums?lat=#{lat}&lng=#{lng}.json"
+  # # get mapbox results
+  # # Return the results as a json
 
   private
 
