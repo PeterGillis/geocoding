12710a968497f74fc1aa4a8d8407e2c37ed9d1f9
Peter Gillis <petergillis@hotmail.com>
2019-08-07 15:29:40 +0200

    back to basics, removed all methods and code but index from controller

--- app/controllers/museums_controller.rb
+++ app/controllers/museums_controller.rb
@@ -1,39 +1,10 @@
 # frozen_string_literal: true
 
 class MuseumsController < ApplicationController
-
   def index
-    @museums = Museum.all
-    # render json:
-    mapbox_key = ENV['MAPBOX_API_KEY']
-    # url = "https://api.mapbox.com/geocoding/v5/mapbox.places/-73.989,40.733.json?types=poi&access_token=#{mapbox_key}"
-    # url = "https://api.mapbox.com/geocoding/v5/mapbox.places/museums?lat=#{latitude}&lng=#{longitude}&access_token=#{mapbox_key}"
-    response = HTTParty.get("https://api.mapbox.com/geocoding/v5/mapbox.places/-73.989,40.733.json?types=poi&access_token=#{mapbox_key}")
-    body = JSON.parse(response.body)
-  #   # response = RestClient.get(url)
-  #   # json = JSON.parse(response)
-  #   # render json: {
-  #   #   request.params[:lat]
-  #   # }
+    # @museums = Museum.all
+    render json: { 'lat': (params[:lat]), 'lng': (params[:lng])}
+    raise
 
-  #   museum_serialized = open(url).read
-  #   museum = JSON.parse(museum_serialized)
-
-  #   puts "#{museum['lat']} - #{museum['access_token']}"
-
-  end
-
-  # # lat = params[:lat]
-  # # lng = params[:lng]
-
-  # # Call mapbox - give the coords + 'museums'
-  # # make json show on screen then make dynamic.
-  # # "https://api.mapbox.com/geocoding/v5/mapbox.places/museums?lat=#{lat}&lng=#{lng}.json"
-  # # get mapbox results
-  # # Return the results as a json
-  private
-
-  def params
-    params.require(:museum).permit(:name, :address, :latitude, :longitude)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,4 @@
 Rails.application.routes.draw do
-  resources :museums, only: [:index] do
-    # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  end
-  # Add a real time route
-  mount ActionCable.server => '/cable'
+  resources :museums, only: [:index]
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
