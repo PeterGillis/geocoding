e004c5a812572dfba2df81ec1973f341b99aa1bb
Peter Gillis <petergillis@hotmail.com>
2019-08-07 17:41:31 +0200

    sudocode added, Poi parameter tested

--- app/controllers/museums_controller.rb
+++ app/controllers/museums_controller.rb
@@ -3,11 +3,29 @@
 class MuseumsController < ApplicationController
   def index
     # @museums = Museum.all
-    render json: { 'lat': (params[:lat]), 'lng': (params[:lng])}
+    render json: { 'lat': params[:lat],
+      'lng': params[:lng],
+      'poi': params[:poi]
+    }
   end
-
-
 end
 
+# reference:
+# objective: show the postcode and the name of the returned results as a JSON
+# render json {
+# postcode: ["name"],
+#
+# }
 
-# objective: show the postcode and the name of the retirned results as a JSON
+# {
+#   "10999": ["Werkbundarchiv  museum of things"],
+#   "12043": ["Museum im Bhmischen Dorf"],
+#   "10179": [
+#     "Mrkisches Museum",
+#     "Museum Kindheit und Jugend",
+#     "Historischer Hafen Berlin"
+#   ],
+#   "12435": ["Archenhold Observatory"]
+# }
+# mapbox_key = ENV['MAPBOX_API_KEY']
+# url = "https://api.mapbox.com/geocoding/v5/mapbox.places/-73.989,40.733.json?types=poi&access_token=#{mapbox_key}"
