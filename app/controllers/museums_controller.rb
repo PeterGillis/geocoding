7b6bbc7c82de105a3df8d02cc20a9eb8aad81abb
Peter Gillis <petergillis@hotmail.com>
2019-08-16 15:52:46 +0200

    more testing, this time using binding.pry returns a longer resposne with more than just the Mapbox blurb

--- app/controllers/museums_controller.rb
+++ app/controllers/museums_controller.rb
@@ -14,25 +14,35 @@ class MuseumsController < ApplicationController
   def find_nearby_places(lat, lng)
     # API call (does not work yet)
     mapbox_key = ENV['MAPBOX_API_KEY']
-    # response = HTTParty.get("https://api.mapbox.com/geocoding/v5/mapbox.places/&lat=#{lat}&lng=#{lng}.json?access_token=#{mapbox_key}")
-    # JSON.parse(response.body)
-    # binding.pry
-    url = "https://api.mapbox.com/geocoding/v5/mapbox.places/&lat=#{lat}&lng=#{lng}.json?access_token=#{mapbox_key}"
-    response = open(url).read
-    hashed_response = JSON.parse(response)
-    return_places(hashed_response)
+    response = HTTParty.get("https://api.mapbox.com/geocoding/v5/mapbox.places/&lat=#{lat}&lng=#{lng}.json?access_token=#{mapbox_key}")
+    JSON.parse(response.body)
+
+    museums_hash = {
+      type: 'museum',
+      postcode: 'postcode',
+      name: ['place_name']
+    }
+
+    JSON.parse(museums_hash)
+
+    # url = "https://api.mapbox.com/geocoding/v5/mapbox.places/&lat=#{lat}&lng=#{lng}.json?access_token=#{mapbox_key}"
+    # response = open(url).read
+    # hashed_response = JSON.parse(response)
+    # return_places(hashed_response)
     # interpolating the lat and long params in the base API URL
     # Using response with url a a parameter to open the URL
     # decalring a variabe for the response and returning it.
-  end
+  # end
 
-  def return_places(response)
-    nearby_places = []
-    response["features, type"].each { |obj| nearby_places << obj["place_name"] }
-    nearby_places
+  # def return_places(response)
+  #   nearby_places = []
+  #   response["features, type"].each { |obj| nearby_places << obj["place_name"] }
+  #   nearby_places
 
     # defining an empty array,
     # pushing the results to ist and filtering by features and type (as an example)
     # calling the method
+
+
   end
 end
